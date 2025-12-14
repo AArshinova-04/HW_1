04c60d76e70861d69e34d70df09c216c8437e88c
Anna Arshinova <anna8603@mail.ru>
2025-12-14 18:11:14 +0400

    Add rate_hw in Student and Reviewer

--- main.py
+++ main.py
@@ -7,6 +7,19 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lecture(self, lecturer, course, grade): # выставляем оценки лектору
+        if (isinstance(lecturer, Lecturer) # Проверяем, что лектор - экземпляр класса Lecturer
+            and course in self.courses_in_progress  # и что студент проходит этот курс
+            and course in lecturer.courses_attached): # и что лектор ведёт этот курс
+            
+            if course in lecturer.grades:
+                    lecturer.grades[course].append(grade)
+            else:
+                lecturer.grades[course] = [grade]
+            return 'Оценка выставлена'    
+        else:
+            return 'Ошибка'    
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
@@ -16,14 +29,22 @@ class Mentor:
 class Lecturer(Mentor): # лекторы
     def __init__(self, name, surname): 
         super().__init__(name, surname)
+        self.grades = {} # Оценки за лекции от студентов
 
 class Reviewer(Mentor): # эксперты, проверяющие домашние задания)
     def __init__(self, name, surname):
         super().__init__(name, surname)                        
 
-lecturer = Lecturer('Иван', 'Иванов')
-reviewer = Reviewer('Пётр', 'Петров')
-print(isinstance(lecturer, Mentor)) # True
-print(isinstance(reviewer, Mentor)) # True
-print(lecturer.courses_attached)    # []
-print(reviewer.courses_attached)    # []
+    def rate_hw(self, student, course, grade): # Только Reviewer может оценивать домашние задания
+        if (isinstance(student, Student) 
+            and course in self.courses_attached 
+            and course in student.courses_in_progress):
+            
+            if course in student.grades:
+                student.grades[course].append(grade)
+            else:
+                student.grades[course] = [grade]
+            return 'Оценка выставлена'    
+        else:
+            return 'Ошибка'
+
