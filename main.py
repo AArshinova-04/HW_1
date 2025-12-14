5de37ffb8e8362190d9755b0c7d75dcfe791223b
Anna Arshinova <anna8603@mail.ru>
2025-12-14 17:56:14 +0400

    Check classes Student, Mentor, Lecture, Reviewer

--- main.py
+++ main.py
@@ -20,3 +20,10 @@ class Lecturer(Mentor): # лекторы
 class Reviewer(Mentor): # эксперты, проверяющие домашние задания)
     def __init__(self, name, surname):
         super().__init__(name, surname)                        
+
+lecturer = Lecturer('Иван', 'Иванов')
+reviewer = Reviewer('Пётр', 'Петров')
+print(isinstance(lecturer, Mentor)) # True
+print(isinstance(reviewer, Mentor)) # True
+print(lecturer.courses_attached)    # []
+print(reviewer.courses_attached)    # []
